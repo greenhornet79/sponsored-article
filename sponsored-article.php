f7255f4565fb0d359e6682a81a646c74e04cf8a2
Jeremy Green <jeremygreen@Jeremys-MacBook-Pro-2.local>
2014-07-26 15:36:10 -0600

    Change namespace to spa

--- sponsored-article.php
+++ sponsored-article.php
@@ -12,41 +12,41 @@
 $plugin = plugin_basename(__FILE__); 
 
 // Add settings link on plugin page
-function na_settings_link($links) { 
-  $settings_link = '<a href="options-general.php?page=native-ad/native-ad.php">Settings</a>'; 
+function spa_settings_link($links) { 
+  $settings_link = '<a href="options-general.php?page=sponsored-article/sponsored-article.php">Settings</a>'; 
   array_unshift($links, $settings_link); 
   return $links; 
 }
-add_filter( "plugin_action_links_$plugin" , 'na_settings_link' );
+add_filter( "plugin_action_links_$plugin" , 'spa_settings_link' );
 
-add_action( 'admin_enqueue_scripts', 'na_enqueue_color_picker' );
-function na_enqueue_color_picker( ) {
+add_action( 'admin_enqueue_scripts', 'spa_enqueue_color_picker' );
+function spa_enqueue_color_picker( ) {
     wp_enqueue_style( 'wp-color-picker' );
-    wp_enqueue_script( 'na-script', plugins_url('script.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+    wp_enqueue_script( 'spa-script', plugins_url('script.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
 }
 
-function na_create_options_page() {
+function spa_create_options_page() {
  
     add_options_page(
-        'Native Ad',           
-        'Native Ad',          
+        'Sponsored Article',           
+        'Sponsored Article',          
         'manage_options',          
         __FILE__,   
-        'na_settings_page'   
+        'spa_settings_page'   
     );
  
 } 
-add_action('admin_menu', 'na_create_options_page');
+add_action('admin_menu', 'spa_create_options_page');
 
-function na_settings_page() {
+function spa_settings_page() {
  	?>
     <div class="wrap">
 
-        <h2>Native Ad Options</h2>
+        <h2>Sponsored Article Options</h2>
 
       	<form method="post" action="options.php">
-            <?php settings_fields( 'na_options' ); ?>
-            <?php do_settings_sections( 'na_opt' ); ?>          
+            <?php settings_fields( 'spa_options' ); ?>
+            <?php do_settings_sections( 'spa_opt' ); ?>          
             <?php submit_button(); ?>
         </form>
 
@@ -56,230 +56,196 @@ function na_settings_page() {
 } 
 
 
-add_action('admin_init', 'na_admin_init');
-function na_admin_init() {
+add_action('admin_init', 'spa_admin_init');
+function spa_admin_init() {
  
 	register_setting(
-        'na_options',
-        'na_options',
-        'na_validate_options'
+        'spa_options',
+        'spa_options',
+        'spa_validate_options'
     );
      
 
     add_settings_section(
-        'na_general',        
+        'spa_general',        
         'General Options',                 
         '',
-        'na_opt'                           
+        'spa_opt'                           
     );
 
     add_settings_field(
-        'na_text',                  
+        'spa_text',                  
         'Sponsored Text',                         
-        'na_text_input',  
-        'na_opt',                         
-        'na_general'       
+        'spa_text_input',  
+        'spa_opt',                         
+        'spa_general'       
     );
 
      add_settings_field(
-        'na_text_color',                  
+        'spa_text_color',                  
         'Sponsored Text Color',                         
-        'na_text_color_input',  
-        'na_opt',                         
-        'na_general'       
+        'spa_text_color_input',  
+        'spa_opt',                         
+        'spa_general'       
     );
 
     add_settings_field(
-        'na_color',                  
+        'spa_color',                  
         'Sponsored Background Color',                         
-        'na_color_input',  
-        'na_opt',                         
-        'na_general'       
+        'spa_color_input',  
+        'spa_opt',                         
+        'spa_general'       
     );
 
      add_settings_field(
-        'na_content_color',                  
-        'Post Background Color',                         
-        'na_content_color_input',  
-        'na_opt',                         
-        'na_general'       
+        'spa_content_color',                  
+        'Content Background Color',                         
+        'spa_content_color_input',  
+        'spa_opt',                         
+        'spa_general'       
     );
 } 
 
-function na_text_input() {
+function spa_text_input() {
 
-	$options = wp_parse_args( get_option( 'na_options' ), array('na_text' => 'Sponsored'));
-	$text = $options['na_text'];
-	echo "<input id='na_text' name='na_options[na_text]' value='$text'>";
+	$options = wp_parse_args( get_option( 'spa_options' ), array('spa_text' => 'Sponsored'));
+	$text = $options['spa_text'];
+	echo "<input id='spa_text' name='spa_options[spa_text]' value='$text'>";
 }
 
-function na_text_color_input() {
+function spa_text_color_input() {
 
-    $options = wp_parse_args( get_option( 'na_options' ), array('na_text_color' => '#ffffff'));
-    $text_color = $options['na_text_color'];
-    echo "<input class='color-picker'  id='na_text_color' name='na_options[na_text_color]' value='$text_color'>";
+    $options = wp_parse_args( get_option( 'spa_options' ), array('spa_text_color' => ''));
+    $text_color = $options['spa_text_color'];
+    echo "<input class='color-picker'  id='spa_text_color' name='spa_options[spa_text_color]' value='$text_color'>";
 }
 
-function na_color_input() {
+function spa_color_input() {
 
-    $options = wp_parse_args( get_option( 'na_options' ), array('na_color' => '#000000'));
-    $color = $options['na_color'];
-    echo "<input class='color-picker' id='na_color' name='na_options[na_color]' value='$color'>";
+    $options = wp_parse_args( get_option( 'spa_options' ), array('spa_color' => ''));
+    $color = $options['spa_color'];
+    echo "<input class='color-picker' id='spa_color' name='spa_options[spa_color]' value='$color'>";
 }
 
-function na_content_color_input() {
+function spa_content_color_input() {
 
-    $options = wp_parse_args( get_option( 'na_options' ), array('na_content_color' => '#ffffff'));
-    $content_color = $options['na_content_color'];
-    echo "<input class='color-picker' id='na_content_color' name='na_options[na_content_color]' value='$content_color'>";
+    $options = wp_parse_args( get_option( 'spa_options' ), array('spa_content_color' => ''));
+    $content_color = $options['spa_content_color'];
+    echo "<input class='color-picker' id='spa_content_color' name='spa_options[spa_content_color]' value='$content_color'>";
 
 }
 
-function na_validate_options( $input ) {
+function spa_validate_options( $input ) {
     return $input;
 }
 
 
 // add meta box to post screen
-add_action( 'add_meta_boxes', 'na_meta_box_create' );
-function na_meta_box_create() {
+add_action( 'add_meta_boxes', 'spa_meta_box_create' );
+function spa_meta_box_create() {
 
-	add_meta_box( 'na-options', 'Sponsored Article', 'na_options_function', 'post', 'side', 'high' );
+	add_meta_box( 'spa-options', 'Sponsored Article', 'spa_options_function', 'post', 'side', 'high' );
 
-    add_meta_box( 'na-options', 'Sponsored Article', 'na_options_function', 'article', 'side', 'high' );
+    add_meta_box( 'spa-options', 'Sponsored Article', 'spa_options_function', 'article', 'side', 'high' );
 
 }
 
-function na_options_function( $post ) {
+function spa_options_function( $post ) {
 
-	$is_ad = get_post_meta( $post->ID, '_na_ad', true );
+	$is_ad = get_post_meta( $post->ID, '_spa_ad', true );
 
 	?>
 
 	<table class="form-table">
 		<tr valign="top">
 			<td>
-				<input id="na_ad" class="widefat" type="checkbox" name="na_ad" <?php checked( 'on', $is_ad ); ?> /> <label for="na_ad">This is a sponsored article</label>
+				<input id="spa_ad" class="widefat" type="checkbox" name="spa_ad" <?php checked( 'on', $is_ad ); ?> /> <label for="spa_ad">This is a sponsored article</label>
 			</td>
 		</tr>
 	</table>
 	<?php 
 }
 
-add_action( 'save_post', 'na_options_save_meta' );
-function na_options_save_meta( $post_id ) {
+add_action( 'save_post', 'spa_options_save_meta' );
+function spa_options_save_meta( $post_id ) {
 
-	if ( !empty( $_POST['na_ad'] ) ) {
-		update_post_meta( $post_id, '_na_ad', $_POST['na_ad'] );
+	if ( !empty( $_POST['spa_ad'] ) ) {
+		update_post_meta( $post_id, '_spa_ad', $_POST['spa_ad'] );
 	} else {
-		update_post_meta( $post_id, '_na_ad', 'off' );
+		update_post_meta( $post_id, '_spa_ad', 'off' );
 	}
 
 }
 
 
 // add the visual indicator to the post title
-add_filter( 'the_title', 'na_ad_post_title');
-function na_ad_post_title ( $title ) {
+add_filter( 'the_title', 'spa_ad_post_title');
+function spa_ad_post_title ( $title ) {
 
     if ( !is_admin() ) { // so the markup won't display in the admin
 
         global $post;
-        $is_ad = get_post_meta( $post->ID, '_na_ad', true );
+        $is_ad = get_post_meta( $post->ID, '_spa_ad', true );
 
-        $options = get_option( 'na_options' );
-        $text = $options['na_text'];
+        $options = get_option( 'spa_options' );
+        $text = $options['spa_text'];
 
-          // this will only show the indicator within the loop, off for now
-          // if( $title == $post->post_title and !is_page() && $is_ad == 'on' && in_the_loop() ){
 
-          //   $title = '<span class="native-ad-title">Sponsored</span> ' . $title;
 
-          // }
-
-            if ( !is_page() && $is_ad == 'on' && $title == $post->post_title ) {
-
-                $title = '<span class="native-ad-indicator">' . $text . '</span> ' . $title;
-            }
+        if ( $is_ad == 'on' && $title == $post->post_title ) {
+            
+            $title = '<span class="spa-indicator">' . $text . '</span> ' . $title;
+        }
     }
-	
 
-
-      
   return $title;
 }
 
 // add a container to the content for styling purposes
-add_filter( 'the_content', 'na_ad_post_content');
-function na_ad_post_content ( $content ) {
+add_filter( 'the_content', 'spa_ad_post_content');
+function spa_ad_post_content ( $content ) {
 
 	global $post;
 
-	$is_ad = get_post_meta( $post->ID, '_na_ad', true );
+	$is_ad = get_post_meta( $post->ID, '_spa_ad', true );
 
     if ( !is_page() && $is_ad == 'on' && in_the_loop() ) {
-
-        $content = '<div class="native-ad-content"> ' . $content. '</div>';
-
+        $content = '<div class="spa-content"> ' . $content. '</div>';
     }
   return $content;
 }
 
 // a class to the body element for styling purposes
-add_filter( 'body_class', 'na_ad_body_classes');
-function na_ad_body_classes ( $classes ) {
+add_filter( 'body_class', 'spa_ad_body_classes');
+function spa_ad_body_classes ( $classes ) {
 
 	global $post;
 
-	$is_ad = get_post_meta( $post->ID, '_na_ad', true );
+	$is_ad = get_post_meta( $post->ID, '_spa_ad', true );
 
     if( !is_page() && $is_ad == 'on' ){
 
-	   $classes[] = 'is-native-ad';
+	   $classes[] = 'is-sponsored-article';
 
 	}
   return $classes;
 }
 
 // add styles for the plugin to the head of the document
-add_action( 'wp_head', 'na_ad_styles' );
-function na_ad_styles() {
+add_action( 'wp_head', 'spa_ad_styles' );
+function spa_ad_styles() {
 
-	$options = get_option( 'na_options' );
-	$color = $options['na_color'];
-    $text_color = $options['na_text_color'];
-    $content_color = $options['na_content_color'];
+	$options = get_option( 'spa_options' );
+	$color = $options['spa_color'];
+    $text_color = $options['spa_text_color'];
+    $content_color = $options['spa_content_color'];
 
 	?>
 	<style type="text/css" media="screen">
-		.native-ad-indicator { color: <?php echo $text_color; ?>; font-size: .8em; background: <?php echo $color; ?>; border-radius: 2px; padding: 0 2px; display: inline-block; z-index: 99;}
-        .native-ad-content {
+		.spa-indicator { color: <?php echo $text_color; ?>; font-size: .8em; background: <?php echo $color; ?>; border-radius: 2px; padding: 0 2px; display: inline-block; z-index: 99;}
+        .spa-content {
             background: <?php echo $content_color; ?>; padding: 10px;
         }
 	</style>
 	<?php 
 }
-
-// add user role for Sponsor, who can edit posts and articles, but not delete them
-// register_activation_hook( __FILE__, 'na_sponsor_role_create' );
-
-// function na_sponsor_role_create() {
-
-//     add_role(
-//         'na_sponsor',
-//         __( 'Sponsor' ),
-//         array(
-//             'read'          => true,
-//             'edit_articles'    => true,
-//             'edit_others_articles' => true,
-//             'edit_published_articles' => true,
-//             'publish_articles' => true,
-//             'edit_others_posts' => true,
-//             'edit_posts'    => true,
-//             'edit_published_posts' => true,
-//             'publish_posts' => true,
-//             'edit_others_posts' => true,
-//             'moderate_comments' => false
-//         )
-//     );
-// }
